218b3734a512c01fc527707e1501286912d7cc26
colekirky17 <colekirky17@gmail.com>
2026-05-22 17:52:55 -0400

    Delete SignatureSwings-minimal-starter directory

--- SignatureSwings-minimal-starter/app/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <main className="flex min-h-screen items-center justify-center bg-white text-black">
-      <h1 className="text-4xl font-semibold">Signature Swings</h1>
-    </main>
-  );
-}
